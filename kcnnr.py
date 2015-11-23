92dbd65a1ef24d04e980a0f48a751147afcf5fa0
Huseyin <coskunh1986@gmail.com>
2015-11-23 18:25:38 +0100

    commit msg

--- kcnnr.py
+++ kcnnr.py
@@ -92,9 +92,9 @@ def train_model(params):
     done_looping = False
     epoch_counter = 0
     best_validation_loss=np.inf
-    print("Mean of training data:%f",np.mean(y_train))
-    print("Mean of val data:%f",np.mean(y_val))
-    print("Mean of test data:%f",np.mean(y_test))
+    print("Mean of training data:%f, abs mean",np.mean(y_train),np.abs(y_train))
+    print("Mean of val data:%f, abs mean: ",np.mean(y_val),np.abs(y_val))
+    print("Mean of test data:%f, abs mean",np.mean(y_test),np.abs(y_test))
     while (epoch_counter < n_epochs) and (not done_looping):
         epoch_counter = epoch_counter + 1
         print("Training model...")
