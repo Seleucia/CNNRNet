2e92ed8c596db24d53b6defb68e6472b06b9b139
Huseyin <coskunh1986@gmail.com>
2015-11-29 05:33:59 +0100

    val distinguished test settings

--- kcnnrBN.py
+++ kcnnrBN.py
@@ -4,74 +4,87 @@ import numpy as np
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.advanced_activations import PReLU
+from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Merge
 from keras.models import Sequential
 from keras.optimizers import Adagrad
 from keras.optimizers import SGD
-
+from keras import regularizers
+import sys
 from helper import config, utils
 
+sys.setrecursionlimit(50000)
+
 def build_model(params):
-
-    lmodel = Sequential()
-
+    l2=regularizers.l2(0.01)
+    l2_out=regularizers.l2(0.0001)
     # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
     # this applies 32 convolution filters of size 3x3 each.
-    lmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0])))
+
+    #########Left Stream######################
+    lmodel = Sequential()
+    lmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0]),init='he_normal', W_regularizer=l2))
     lmodel.add(PReLU())
     lmodel.add(MaxPooling2D(pool_size=(2, 2)))
-    lmodel.add(Dropout(0.25))
-    lmodel.add(Convolution2D(params["nkerns"][1], 3, 3))
+    lmodel.add(BatchNormalization())
+
+
+    lmodel.add(Convolution2D(params["nkerns"][1], 3, 3,init='he_normal', W_regularizer=l2))
     lmodel.add(PReLU())
     lmodel.add(MaxPooling2D(pool_size=(2, 2)))
-    lmodel.add(Dropout(0.25))
-    lmodel.add(Convolution2D(params["nkerns"][2], 3, 3))
+    lmodel.add(BatchNormalization())
+
+    lmodel.add(Convolution2D(params["nkerns"][2], 2, 2,init='he_normal', W_regularizer=l2))
     lmodel.add(PReLU())
     lmodel.add(MaxPooling2D(pool_size=(2, 2)))
-    lmodel.add(Dropout(0.25))
+    lmodel.add(BatchNormalization())
 
 
     lmodel.add(Flatten())
-    lmodel.add(Dense(200))
+    lmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
     lmodel.add(PReLU())
-    lmodel.add(Dropout(0.5))
-    lmodel.add(Dense(200))
+    lmodel.add(BatchNormalization())
+
+    lmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
     lmodel.add(PReLU())
 
+    #########Right Stream######################
     rmodel = Sequential()
-    # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
-    # this applies 32 convolution filters of size 3x3 each.
-    rmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0])))
+    rmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0]),init='he_normal', W_regularizer=l2))
     rmodel.add(PReLU())
     rmodel.add(MaxPooling2D(pool_size=(2, 2)))
-    rmodel.add(Dropout(0.25))
-    rmodel.add(Convolution2D(params["nkerns"][1], 3, 3))
+    lmodel.add(BatchNormalization())
+
+    rmodel.add(Convolution2D(params["nkerns"][1], 3, 3,init='he_normal', W_regularizer=l2))
     rmodel.add(PReLU())
     rmodel.add(MaxPooling2D(pool_size=(2, 2)))
-    rmodel.add(Dropout(0.25))
-    rmodel.add(Convolution2D(params["nkerns"][3], 3, 3))
+    lmodel.add(BatchNormalization())
+
+    rmodel.add(Convolution2D(params["nkerns"][3], 2, 2,init='he_normal', W_regularizer=l2))
     rmodel.add(PReLU())
     rmodel.add(MaxPooling2D(pool_size=(2, 2)))
-    rmodel.add(Dropout(0.25))
+    lmodel.add(BatchNormalization())
 
     rmodel.add(Flatten())
-    rmodel.add(Dense(200))
+    rmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
     rmodel.add(PReLU())
-    rmodel.add(Dropout(0.5))
-    rmodel.add(Dense(200))
-    rmodel.add(PReLU())
+    lmodel.add(BatchNormalization())
 
+    rmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
+    rmodel.add(PReLU())
+    lmodel.add(BatchNormalization())
+
+    #########Merge Stream######################
     model = Sequential()
     model.add(Merge([lmodel, rmodel], mode='mul'))
-    model.add(Dropout(0.5))
-    model.add(Dense(400))
+    model.add(Dense(400,init='he_normal', W_regularizer=l2_out))
     model.add(PReLU())
-    model.add(Dropout(0.5))
-    model.add(Dense(400))
-    model.add(Activation('linear'))
-    model.add(Dropout(0.5))
+    lmodel.add(BatchNormalization())
 
-    model.add(Dense(3))
+    model.add(Dense(400,init='he_normal'))
+    model.add(Activation('linear'))
+
+    model.add(Dense(3,init='he_normal'))
 
     sgd = SGD(lr=params['initial_learning_rate'], decay=params['learning_rate_decay'], momentum=params['momentum'], nesterov=True)
     adagrad=Adagrad(lr=params['initial_learning_rate'], epsilon=1e-6)
@@ -83,8 +96,6 @@ def train_model(params):
     im_type=params["im_type"]
     batch_size =params["batch_size"]
     n_epochs =params["n_epochs"]
-    nc =params["nc"]  # number of channcels
-    size =params["size"]  # size = [480,640] orijinal size,[height,width]
 
     datasets = data_loader.load_data(params)
     utils.start_log(datasets,params)
@@ -100,10 +111,6 @@ def train_model(params):
     n_train_batches /= batch_size
     n_valid_batches /= batch_size
     n_test_batches /= batch_size
-
-    # n_train_batches = 3
-    # n_valid_batches = 1
-    # n_test_batches = 1
 
     y_val_mean=np.mean(y_val)
     y_val_abs_mean=np.mean(np.abs(y_val))
@@ -128,8 +135,8 @@ def train_model(params):
                 print 'training @ iter = ', iter
 
             Fx = X_train[minibatch_index * batch_size: (minibatch_index + 1) * batch_size]
-            data_Fx = dt_utils.load_batch_images(size, nc, "F", Fx,im_type)
-            data_Sx = dt_utils.load_batch_images(size, nc, "S", Fx,im_type)
+            data_Fx = dt_utils.load_batch_images(params,"F", Fx)
+            data_Sx = dt_utils.load_batch_images(params,"S", Fx)
             data_y = y_train[minibatch_index * batch_size: (minibatch_index + 1) * batch_size]
             loss =model.train_on_batch([data_Fx, data_Sx], data_y)
             s='TRAIN--> epoch %i | minibatch %i/%i | error %f'%(epoch_counter, minibatch_index + 1, n_train_batches,  loss)
@@ -140,10 +147,10 @@ def train_model(params):
         print("Validating model...")
         this_validation_loss = 0
         for i in xrange(n_valid_batches):
-            Fx = X_train[i * batch_size: (i + 1) * batch_size]
-            data_Fx = dt_utils.load_batch_images(size, nc, "F", Fx,im_type)
-            data_Sx = dt_utils.load_batch_images(size, nc, "S", Fx,im_type)
-            data_y = y_train[i * batch_size: (i + 1) * batch_size]
+            Fx = X_val[i * batch_size: (i + 1) * batch_size]
+            data_Fx = dt_utils.load_batch_images(params,"F", Fx)
+            data_Sx = dt_utils.load_batch_images(params,"S", Fx)
+            data_y = y_val[i * batch_size: (i + 1) * batch_size]
             this_validation_loss += model.test_on_batch([data_Fx, data_Sx],data_y)
             if(check_mode==1):
                 break
@@ -153,15 +160,15 @@ def train_model(params):
         utils.log_write(s,params)
         if this_validation_loss < best_validation_loss:
             best_validation_loss = this_validation_loss
-            ext=params["models"]+str(rn_id)+"_"+str(epoch_counter % 3)+"_"+im_type+".h5"
+            ext=params["models"]+str(rn_id)+"_"+str(epoch_counter % 3)+"_"+im_type+".hdf5"
             model.save_weights(ext, overwrite=True)
-            if(test_counter%params["test_freq"]==0):
-                test_counter+=1
+            test_counter+=1
+            if(test_counter%params["test_freq"]==1):
                 test_losses = 0
                 for i in xrange(n_test_batches):
                     Fx = X_test[i * batch_size: (i + 1) * batch_size]
-                    data_Fx = dt_utils.load_batch_images(size, nc, "F", Fx,im_type)
-                    data_Sx = dt_utils.load_batch_images(size, nc, "S", Fx,im_type)
+                    data_Fx = dt_utils.load_batch_images(params,"F", Fx)
+                    data_Sx = dt_utils.load_batch_images(params,"S", Fx)
                     data_y = y_test[i * batch_size: (i + 1) * batch_size]
                     test_losses +=  model.test_on_batch([data_Fx, data_Sx],data_y)
                     if(check_mode==1):
@@ -171,15 +178,17 @@ def train_model(params):
                 utils.log_write(s,params)
         else:
             if(epoch_counter % 5==0):
-                ext=params["models"]+str(rn_id)+"_regular_"+str(epoch_counter % 5)+"_"+im_type+".h5"
+                ext=params["models"]+str(rn_id)+"_regular_"+str(epoch_counter % 5)+"_"+im_type+".hdf5"
                 model.save_weights(ext, overwrite=True)
 
         if(check_mode==1):
                 break
-    ext=params["models"]+"last_"+str(rn_id)+utils.get_time()+"_"+im_type+".h5"
+    ext=params["models"]+str(rn_id)+"_regular_"+str(epoch_counter % 5)+"_"+im_type+".hdf5"
     model.save_weights(ext, overwrite=True)
     utils.log_write("Training ended",params)
 
 if __name__ == "__main__":
     params= config.get_params()
+    params["rn_id"]="batchnorm_l2" #running id
+    params["notes"]="Batch normalaziton added, no dropout but l2 regularize added, weight init setting norm" #running id
     train_model(params)
