ee6b46a55b8e14eb887aff3aac0c7500f173bcb4
Huseyin <coskunh1986@gmail.com>
2015-12-01 02:31:18 +0100

    deleted dropout

--- kcnnr.py
+++ kcnnr.py
@@ -2,7 +2,7 @@ import helper.data_loader as data_loader
 import helper.dt_utils as dt_utils
 import numpy as np
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.core import Dense, Activation, Flatten
 from keras.layers.advanced_activations import PReLU
 from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Merge
@@ -26,23 +26,19 @@ def build_model(params):
    lmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0]),init='he_normal', W_regularizer=l2))
    lmodel.add(PReLU())
    lmodel.add(MaxPooling2D(pool_size=(2, 2)))
-   lmodel.add(Dropout(0.1))
 
    lmodel.add(Convolution2D(params["nkerns"][1], 3, 3,init='he_normal', W_regularizer=l2))
    lmodel.add(PReLU())
    lmodel.add(MaxPooling2D(pool_size=(2, 2)))
-   lmodel.add(Dropout(0.25))
 
    lmodel.add(Convolution2D(params["nkerns"][2], 2, 2,init='he_normal', W_regularizer=l2))
    lmodel.add(PReLU())
    lmodel.add(MaxPooling2D(pool_size=(2, 2)))
-   lmodel.add(Dropout(0.5))
 
 
    lmodel.add(Flatten())
    lmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
    lmodel.add(PReLU())
-   lmodel.add(Dropout(0.5))
 
    lmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
    lmodel.add(PReLU())
@@ -52,22 +48,18 @@ def build_model(params):
    rmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0]),init='he_normal', W_regularizer=l2))
    rmodel.add(PReLU())
    rmodel.add(MaxPooling2D(pool_size=(2, 2)))
-   rmodel.add(Dropout(0.1))
 
    rmodel.add(Convolution2D(params["nkerns"][1], 3, 3,init='he_normal', W_regularizer=l2))
    rmodel.add(PReLU())
    rmodel.add(MaxPooling2D(pool_size=(2, 2)))
-   rmodel.add(Dropout(0.25))
 
    rmodel.add(Convolution2D(params["nkerns"][3], 2, 2,init='he_normal', W_regularizer=l2))
    rmodel.add(PReLU())
    rmodel.add(MaxPooling2D(pool_size=(2, 2)))
-   rmodel.add(Dropout(0.5))
 
    rmodel.add(Flatten())
    rmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
    rmodel.add(PReLU())
-   rmodel.add(Dropout(0.5))
 
    rmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
    rmodel.add(PReLU())
@@ -85,6 +77,7 @@ def build_model(params):
 
    sgd = SGD(lr=params['initial_learning_rate'], decay=params['learning_rate_decay'], momentum=params['momentum'], nesterov=True)
    adagrad=Adagrad(lr=params['initial_learning_rate'], epsilon=1e-6)
+   model.compile(loss='mean_squared_error', optimizer=adagrad)
    model.compile(loss='mean_squared_error', optimizer=adagrad)
    return model
 
--- mlpr.py
+++ mlpr.py
@@ -1,197 +1,190 @@
-import numpy
-import theano
-import theano.tensor as T
-from theano import shared
+import helper.data_loader as data_loader
+import helper.dt_utils as dt_utils
+import numpy as np
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.advanced_activations import PReLU
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import Merge
+from keras.models import Sequential
+from keras.optimizers import Adagrad
+from keras.optimizers import SGD
+from keras import regularizers
+import sys
+from helper import config, utils
 
-from layers.HiddenLayer import HiddenLayer
-from layers.OutputLayer import OutputLayer
+sys.setrecursionlimit(50000)
 
-# start-snippet-1
+def build_model(params):
+   l2=regularizers.l2(0.000)
+   l2_out=regularizers.l2(0.00000)
+   # input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
+   # this applies 32 convolution filters of size 3x3 each.
 
-theano.config.exception_verbosity = 'high'
+   #########Left Stream######################
+   lmodel = Sequential()
+   lmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0]),init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(MaxPooling2D(pool_size=(2, 2)))
+   lmodel.add(Dropout(0.1))
 
-class MLPRNet(object):
-    def __init__(self,
-                 rng,
-                 input,
-                 batch_size,
-                 nc,
-                 Fx,
-                 Sx):
+   lmodel.add(Convolution2D(params["nkerns"][1], 3, 3,init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(MaxPooling2D(pool_size=(2, 2)))
+   lmodel.add(Dropout(0.25))
 
-        print("Done")
-
-        self.layers = []
-        self.params = []
-
-        Flayer1 = HiddenLayer(
-            rng,
-            input=Fx,
-            n_in=numpy.shape(Fx)[0]*numpy.shape(Fx)[0],
-            n_out=200
-        )
-
-        Slayer1 = HiddenLayer(
-            rng,
-            input=Sx,
-            n_in=numpy.shape(Sx)[0]*numpy.shape(Sx)[0],
-            n_out=200
-        )
-
-        self.layers.append(Flayer1)
-        self.layers.append(Slayer1)
-
-        Flayer2 = HiddenLayer(
-            rng,
-            input=Flayer1.output,
-            n_in=200,
-            n_out=200
-        )
-
-        Slayer2 = HiddenLayer(
-            rng,
-            input=Slayer1.output,
-            n_in=200,
-            n_out=200
-        )
-
-        self.layers.append(Flayer2)
-        self.layers.append(Slayer2)
-
-        Clayer1 = HiddenLayer(
-            rng,
-            input=T.concatenate([Flayer2.output, Slayer2.output], axis=1),
-            n_in=400,
-            n_out=200
-        )
-        self.layers.append(Clayer1)
-
-        Clayer2 = HiddenLayer(
-            rng,
-            input=Clayer1.output,
-            n_in=200,
-            n_out=200
-        )
-        self.layers.append(Clayer2)
-
-        Clayer3 = HiddenLayer(
-            rng,
-            input=Clayer2.output,
-            n_in=200,
-            n_out=200
-        )
-        self.layers.append(Clayer3)
-
-        layerOutput = OutputLayer(input=Clayer3.output, n_in=200, n_out=3)
-        self.layers.append(layerOutput)
-
-        for layer in zip(reversed(self.layers)):
-            self.params.append(layer[0].W)
-            self.params.append(layer[0].b)
-
-        L1 = shared(0.)
-        for param in self.params:
-            L1 += (T.sum(abs(param[0]))+T.sum(abs(param[1])))
-        self.L1=L1
-
-        L2_sqr = shared(0.)
-        for param in self.params:
-            L2_sqr += (T.sum(param[0] ** 2)+T.sum(param[1] ** 2))
-        self.L2_sqr=L2_sqr
+   lmodel.add(Convolution2D(params["nkerns"][2], 2, 2,init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(MaxPooling2D(pool_size=(2, 2)))
+   lmodel.add(Dropout(0.5))
 
 
+   lmodel.add(Flatten())
+   lmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(Dropout(0.5))
 
-        self.y_pred = self.layers[-1].y_pred
-        self.mse = self.layers[-1].mse
-        self.errors = self.layers[-1].errors
+   lmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
 
+   #########Right Stream######################
+   rmodel = Sequential()
+   rmodel.add(Convolution2D(params["nkerns"][0], 3, 3, border_mode='full', input_shape=(params["nc"], params["size"][1], params["size"][0]),init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(MaxPooling2D(pool_size=(2, 2)))
+   rmodel.add(Dropout(0.1))
 
-    def updateparams(self, newparams):
-        def inplaceupdate(x, new):
-            x[...] = new
-            return x
+   rmodel.add(Convolution2D(params["nkerns"][1], 3, 3,init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(MaxPooling2D(pool_size=(2, 2)))
+   rmodel.add(Dropout(0.25))
 
-        paramscounter = 0
-        for p in self.params:
-            pshape = p.get_value().shape
-            pnum = numpy.prod(pshape)
-            p.set_value(inplaceupdate(p.get_value(borrow=True), newparams[paramscounter:paramscounter+pnum].reshape(*pshape)), borrow=True)
-            paramscounter += pnum
+   rmodel.add(Convolution2D(params["nkerns"][3], 2, 2,init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(MaxPooling2D(pool_size=(2, 2)))
+   rmodel.add(Dropout(0.5))
 
-    def get_params(self):
-        return numpy.concatenate([p.get_value().flatten() for p in self.params])
+   rmodel.add(Flatten())
+   rmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(Dropout(0.5))
 
-    def save(self, filename):
-        numpy.save(filename, self.get_params())
+   rmodel.add(Dense(200,init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
 
-    def load(self, filename):
-        self.updateparams(numpy.load(filename))
+   #########Merge Stream######################
+   model = Sequential()
+   model.add(Merge([lmodel, rmodel], mode='mul'))
+   model.add(Dense(400,init='he_normal', W_regularizer=l2_out))
+   model.add(PReLU())
+
+   model.add(Dense(400,init='he_normal'))
+   model.add(Activation('linear'))
+
+   model.add(Dense(3,init='he_normal'))
+
+   sgd = SGD(lr=params['initial_learning_rate'], decay=params['learning_rate_decay'], momentum=params['momentum'], nesterov=True)
+   adagrad=Adagrad(lr=params['initial_learning_rate'], epsilon=1e-6)
+   model.compile(loss='mean_squared_error', optimizer=adagrad)
+   return model
 
 def train_model(params):
-    rn_id=params["rn_id"]
-    im_type=params["im_type"]
-    nc =params["nc"]  # number of channcels
-    size =params["size"]  # size = [480,640] orijinal size,[height,width]
+   rn_id=params["rn_id"]
+   im_type=params["im_type"]
+   batch_size =params["batch_size"]
+   n_epochs =params["n_epochs"]
 
-    # Conv an Pooling parameters
-    nkerns =params["nkerns"]
-    kern_mat =params["kern_mat"]
-    pool_mat =params["pool_mat"]
+   datasets = data_loader.load_data(params)
+   utils.start_log(datasets,params)
 
-    # learning parameters
-    batch_size =params["batch_size"]
-    n_epochs =params["n_epochs"]
-    initial_learning_rate =params["initial_learning_rate"]
-    learning_rate_decay =params["learning_rate_decay"]
-    squared_filter_length_limit =params["squared_filter_length_limit"]
-    learning_rate = theano.shared(numpy.asarray(initial_learning_rate, dtype=theano.config.floatX))
-    lambda_1 = params["lambda_1"]  # regulizer param
-    lambda_2 = params["lambda_2"]
+   X_train, y_train,overlaps_train = datasets[0]
+   X_val, y_val,overlaps_val = datasets[1]
+   X_test, y_test,overlaps_test = datasets[2]
 
-    #### the params for momentum
-    mom_start =params["mom_start"]
-    mom_end = params["mom_end"]
-    # for epoch in [0, mom_epoch_interval], the momentum increases linearly
-    # from mom_start to mom_end. After mom_epoch_interval, it stay at mom_end
-    mom_epoch_interval =params["mom_epoch_interval"]
+   # compute number of minibatches for training, validation and testing
+   n_train_batches = len(X_train)
+   n_valid_batches = len(X_val)
+   n_test_batches = len(X_test)
+   n_train_batches /= batch_size
+   n_valid_batches /= batch_size
+   n_test_batches /= batch_size
 
-    # early-stopping parameters
-    patience = params["patience"]  # look as this many examples regardless
-    patience_increase = params["patience_increase"]  # wait this much longer when a new best is
-    # found
-    improvement_threshold = params["improvement_threshold"]  # a relative improvement of this much is
+   y_val_mean=np.mean(y_val)
+   y_val_abs_mean=np.mean(np.abs(y_val))
+   y_test_mean=np.mean(y_test)
+   y_test_abs_mean=np.mean(np.abs(y_test))
 
+   utils.log_write("Model build started",params)
+   model=  build_model(params)
+   check_mode=params["check_mode"]
+   utils.log_write("Model build ended",params)
+   utils.log_write("Training started",params)
+   done_looping = False
+   epoch_counter = 0
+   best_validation_loss=np.inf
+   test_counter=0
+   while (epoch_counter < n_epochs) and (not done_looping):
+       epoch_counter = epoch_counter + 1
+       print("Training model...")
+       for minibatch_index in xrange(n_train_batches):
+           iter = (epoch_counter - 1) * n_train_batches + minibatch_index
+           if iter % 100 == 0:
+               print 'training @ iter = ', iter
 
+           Fx = X_train[minibatch_index * batch_size: (minibatch_index + 1) * batch_size]
+           data_Fx = dt_utils.load_batch_images(params,"F", Fx)
+           data_Sx = dt_utils.load_batch_images(params,"S", Fx)
+           data_y = y_train[minibatch_index * batch_size: (minibatch_index + 1) * batch_size]
+           loss =model.train_on_batch([data_Fx, data_Sx], data_y)
+           s='TRAIN--> epoch %i | minibatch %i/%i | error %f'%(epoch_counter, minibatch_index + 1, n_train_batches,  loss)
+           utils.log_write(s,params)
+           if(check_mode==1):
+               break
+
+       print("Validating model...")
+       this_validation_loss = 0
+       for i in xrange(n_valid_batches):
+           Fx = X_val[i * batch_size: (i + 1) * batch_size]
+           data_Fx = dt_utils.load_batch_images(params,"F", Fx)
+           data_Sx = dt_utils.load_batch_images(params,"S", Fx)
+           data_y = y_val[i * batch_size: (i + 1) * batch_size]
+           this_validation_loss += model.test_on_batch([data_Fx, data_Sx],data_y)
+           if(check_mode==1):
+               break
+       this_validation_loss /=n_valid_batches
+
+       s ='VAL--> epoch %i | error %f | data mean/abs %f/%f'%(epoch_counter, this_validation_loss,y_val_mean,y_val_abs_mean)
+       utils.log_write(s,params)
+       ext=params["models"]+str(rn_id)+"_"+str(epoch_counter)+"_"+im_type+".hdf5"
+       model.save_weights(ext, overwrite=True)
+       if this_validation_loss < best_validation_loss:
+           best_validation_loss = this_validation_loss
+           test_counter+=1
+           if(test_counter%params["test_freq"]==1):
+               test_losses = 0
+               for i in xrange(n_test_batches):
+                   Fx = X_test[i * batch_size: (i + 1) * batch_size]
+                   data_Fx = dt_utils.load_batch_images(params,"F", Fx)
+                   data_Sx = dt_utils.load_batch_images(params,"S", Fx)
+                   data_y = y_test[i * batch_size: (i + 1) * batch_size]
+                   test_losses +=  model.test_on_batch([data_Fx, data_Sx],data_y)
+                   if(check_mode==1):
+                       break
+               test_losses/=n_test_batches
+               s ='TEST--> epoch %i | error %f | data mean/abs %f / %f' %(epoch_counter, test_losses,y_test_mean,y_test_abs_mean)
+               utils.log_write(s,params)
+       else:
+           if(epoch_counter % 5==0):
+               ext=params["models"]+str(rn_id)+"_regular_"+str(epoch_counter % 5)+"_"+im_type+".hdf5"
+               model.save_weights(ext, overwrite=True)
+
+       if(check_mode==1):
+               break
+   ext=params["models"]+str(rn_id)+"_regular_"+str(epoch_counter % 5)+"_"+im_type+".hdf5"
+   model.save_weights(ext, overwrite=True)
+   utils.log_write("Training ended",params)
 
 if __name__ == "__main__":
+   params= config.get_params()
+   train_model(params)
 
-    params={}
-    params["rn_id"]=1 #running id
-    # early-stopping parameters
-    params['patience']= 10000  # look as this many examples regardless
-    params['patience_increase']=2  # wait this much longer when a new best is
-    params['improvement_threshold']=0.995  # a relative improvement of this much is
-
-    # learning parameters
-    params['lambda_1']= 0.01  # regulizer param
-    params['lambda_2']=0.01  # regulizer param
-    params['mom_start']=0.0005  # the params for momentum
-    params['mom_end']=0.0005    # the params for momentum
-    params['mom_epoch_interval']=500 #for epoch in [0, mom_epoch_interval], the momentum increases linearly from mom_start to mom_end. After mom_epoch_interval, it stay at mom_end
-    params['initial_learning_rate']=0.0005
-    params['learning_rate_decay']= 0.998
-    params['squared_filter_length_limit']=15.0
-    params['batch_size']=120
-    params['n_epochs']=3000
-
-    # dataset parameters
-    params['dataset']="/home/coskun/PycharmProjects/data/rgbd_dataset_freiburg3_large_cabinet/"
-    params['im_type']="gray"
-    params['step_size']=[1,2,5,7,10,12,13,15,16,18,20,21,23,24,25]
-    params['size']=[160, 120] #[width,height]
-    params['nc']=1 #number of dimensions
-    params['multi']=10 #ground truth location differences will be multiplied with this number
-    params['test_size']=0.20 #Test size
-    params['val_size']=0.20 #Test size
-
-    train_model(params)
