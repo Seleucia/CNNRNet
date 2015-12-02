533d9661d63cd1c11a7af28ee8c779322ce2d4f6
Huseyin <coskunh1986@gmail.com>
2015-12-02 20:57:32 +0100

    ...

---
+++ models/bnmlpr.py
@@ -0,0 +1,51 @@
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.advanced_activations import PReLU
+from keras.layers.core import Merge
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+from keras.optimizers import Adagrad
+from keras import regularizers
+
+def build_model(params):
+   l2=regularizers.l2(0.000)
+   l2_out=regularizers.l2(0.00000)
+   dims=4096
+
+   #########Left Stream######################
+   lmodel = Sequential()
+   lmodel.add(Dense(256, input_shape=(dims,),init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(BatchNormalization())
+   lmodel.add(Dense(256,init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(BatchNormalization())
+   lmodel.add(Dense(256,init='he_normal', W_regularizer=l2))
+   lmodel.add(PReLU())
+   lmodel.add(BatchNormalization())
+
+   #########Right Stream######################
+   rmodel = Sequential()
+   rmodel.add(Dense(256, input_shape=(dims,),init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(BatchNormalization())
+   rmodel.add(Dense(256,init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(BatchNormalization())
+   rmodel.add(Dense(256,init='he_normal', W_regularizer=l2))
+   rmodel.add(PReLU())
+   rmodel.add(BatchNormalization())
+
+   #########Merge Stream######################
+   model = Sequential()
+   model.add(Merge([lmodel, rmodel], mode='mul'))
+   model.add(Dense(256,init='he_normal', W_regularizer=l2_out))
+   model.add(PReLU())
+
+   model.add(Dense(256,init='he_normal'))
+   model.add(Activation('linear'))
+
+   model.add(Dense(3,init='he_normal'))
+
+   adagrad=Adagrad(lr=params['initial_learning_rate'], epsilon=1e-6)
+   model.compile(loss='mean_squared_error', optimizer=adagrad)
+   return model
