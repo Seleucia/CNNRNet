55b54818946d57af249668a2207c0a108f45c440
Huseyin <coskunh1986@gmail.com>
2015-12-07 13:04:44 +0100

    7 DoF estimated, indeed this is parametric

--- plot/show_log.py
+++ plot/show_log.py
@@ -7,7 +7,6 @@ params["log_file"]="dcnnr_dr_05_44_52_462880.txt"
 
 model="VAL"
 list_val=utils.log_read(model,params)
-list_val=list_val[1:]
 plot_data.plot_val(list_val,params["wd"]+"/"+"logs/img/"+params["log_file"].replace(".txt",".png"))
 
 list_val=utils.log_read_train(params)
